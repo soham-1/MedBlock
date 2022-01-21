60b89f5414d2939fa31f139cf5a77f5fdd7ab575
soham-1 <sohampatkar93@gmail.com>
2022-01-21 17:06:48 +0530

    insurer ui changes

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,8 @@
 
 var web3;
 
-var agentContractAddress = '0x3008eaf4e51aF0389474215018513ee31d846dA4';
-//var agentContractAddress = '0x535f855ca84E4BAc02333704d5accf65831BC50d';
+// var agentContractAddress = '0x3008eaf4e51aF0389474215018513ee31d846dA4';
+var agentContractAddress = '0xaDA913a4f625c30a92878876bBf160F73c137320';
 
 
 function connect(){
