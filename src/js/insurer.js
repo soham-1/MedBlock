a8df4a0cd73c5b6ff7d5ee9f942a8a3a4e06f4b6
Hemant <yadavhemant1716@gmail.com>
2022-01-26 14:17:18 +0530

    insurer ui updated

--- src/js/insurer.js
+++ src/js/insurer.js
@@ -1,5 +1,5 @@
-$('title').load('common/title.html');
-$("#insurer_sidebar").load('common/insurer_sidebar.html');
+$('title').load('../common/title.html');
+$("#insurer_sidebar").load('../common/insurer_sidebar.html');
 
 var key;
 var ipfs = window.IpfsApi('localhost', '5001')
@@ -10,5 +10,5 @@ $(window).on('load', function() {
     connect();
     key = web3.currentProvider.selectedAddress;
     key = key.toLocaleLowerCase();
-    
+
 });
